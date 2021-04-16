b48ba0d1c67b6c7f181318cd797a72303a9f7a0b
Roy Park <roypark2638@gmail.com>
2021-04-15 17:06:47 -0700

    Setup basic Authmanager, imported app log and launching image.

--- Instagram2/Controllers/Other/SignInViewController.swift
+++ Instagram2/Controllers/Other/SignInViewController.swift
@@ -12,7 +12,9 @@ class SignInViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        title = "Sign in"
+        view.backgroundColor = .systemBackground
+        
     }
     
 
--- Instagram2/Managers/AuthManager.swift
+++ Instagram2/Managers/AuthManager.swift
@@ -14,4 +14,50 @@ final class AuthManager {
     private init() {}
     
     let auth = Auth.auth()
+    
+    public var isSignedIn: Bool {
+        return auth.currentUser != nil
+    }
+    
+    
+    /// Signing the user with the email method
+    /// - Parameters:
+    ///   - email: String user email
+    ///   - password: String user password
+    ///   - completion: Tell the caller what is the result.
+    public func signIn(
+        email: String,
+        password: String,
+        completion: @escaping (Result<User, Error>) -> Void
+    ) {
+        
+    }
+    
+    /// Signing the user with the email method
+    /// - Parameters:
+    ///   - email: String user email
+    ///   - username: String username
+    ///   - password: String user password
+    ///   - profilePicture: Data? user profile image
+    ///   - completion: Tell the caller what is the result.
+    public func signUp(
+        email: String,
+        username: String,
+        password: String,
+        profilePicture: Data?,
+        completion: @escaping (Result<User, Error>) -> Void
+    ) {
+        
+    }
+    
+    /// Signing out the user from the app
+    /// - Parameter completion: We want to know if the user successfully signed out or not
+    public func signOut(completion: @escaping (Bool) -> Void) {
+        do {
+            try auth.signOut()
+        }
+        catch {
+            
+        }
+    }
 }
