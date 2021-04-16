a11c13499dca4121dd5d6a404f4bc401fa421deb
Roy Park <roypark2638@gmail.com>
2021-04-15 23:38:18 -0700

    Added UIView extension for constraints

---
+++ Instagram2/Resources/Extensions.swift
@@ -0,0 +1,36 @@
+//
+//  Extensions.swift
+//  Instagram2
+//
+//  Created by Roy Park on 4/15/21.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIView {
+    var top: CGFloat {
+        frame.origin.y
+    }
+    
+    var bottom: CGFloat {
+        frame.origin.y+height
+    }
+    
+    var left: CGFloat {
+        frame.origin.x
+    }
+    
+    var right: CGFloat {
+        frame.origin.x+width
+    }
+    
+    var width: CGFloat {
+        frame.size.width
+    }
+    
+    var height: CGFloat {
+        frame.size.height
+    }
+}
