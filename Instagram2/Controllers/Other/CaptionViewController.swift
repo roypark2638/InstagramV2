36ac62a2a1499d33d840f752c6c8e206749e0899
Roy Park <roypark2638@gmail.com>
2021-04-18 13:37:35 -0700

    Added filter button and display the filtered image on the preview. Added next button to navigate to the Adding caption vc

--- Instagram2/Controllers/Other/CaptionViewController.swift
+++ Instagram2/Controllers/Other/CaptionViewController.swift
@@ -8,6 +8,17 @@
 import UIKit
 
 class CaptionViewController: UIViewController {
+    
+    private let image: UIImage
+    
+    init(image: UIImage) {
+        self.image = image
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- Instagram2/Controllers/Other/PostEditViewController.swift
+++ Instagram2/Controllers/Other/PostEditViewController.swift
@@ -10,11 +10,31 @@ import CoreImage
 
 class PostEditViewController: UIViewController {
     
+    fileprivate var filters = [UIImage]()
+    
     private let imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.clipsToBounds = true
         imageView.contentMode = .scaleAspectFill
+        imageView.tintColor = .label
         return imageView
+    }()
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 2
+        layout.sectionInset = UIEdgeInsets(top: 1, left: 10, bottom: 1, right: 10)
+        let collectionView = UICollectionView(
+            frame: .zero,
+            collectionViewLayout: layout)
+        collectionView.backgroundColor = .secondarySystemBackground
+        
+        collectionView.register(
+            PhotoCollectionViewCell.self,
+            forCellWithReuseIdentifier: PhotoCollectionViewCell.identifier)
+        return collectionView
     }()
     
     private let image: UIImage
@@ -34,6 +54,18 @@ class PostEditViewController: UIViewController {
         title = "Edit"
         imageView.image = image
         view.addSubview(imageView)
+        view.addSubview(collectionView)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        configureFilters()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Next",
+            style: .done,
+            target: self,
+            action: #selector(didTapNext)
+        )
         
         DispatchQueue.main.asyncAfter(deadline: .now()+4) {
             self.filterImage(image: self.image)
@@ -48,6 +80,18 @@ class PostEditViewController: UIViewController {
             width: view.width,
             height: view.width
         )
+        
+        collectionView.frame = CGRect(
+            x: 0,
+            y: imageView.bottom+20,
+            width: view.width,
+            height: 100
+        )
+    }
+    
+    private func configureFilters() {
+        guard let filterIcon = UIImage(systemName: "camera.filters") else { return }
+        filters.append(filterIcon)
     }
     
     private func filterImage(image: UIImage) {
@@ -68,5 +112,43 @@ class PostEditViewController: UIViewController {
             imageView.image = filteredImage
         }
     }
+    
+    @objc private func didTapNext() {
+        let vc = CaptionViewController(image: image)
+        vc.title = "Add Caption"
+        navigationController?.pushViewController(vc, animated: true)
+    }
 
 }
+
+// MARK: - UICollectionViewDataSource
+extension PostEditViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return filters.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: PhotoCollectionViewCell.identifier,
+            for: indexPath
+        ) as? PhotoCollectionViewCell
+        else {
+            fatalError()
+        }
+        cell.configure(with: filters[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        filterImage(image: image)
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+extension PostEditViewController: UICollectionViewDelegate {
+    
+}
+
+
